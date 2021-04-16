ccc29720d838346c2d99b3367a3f5b2911f7bef4
Andrew Olson <frrrkin@gmail.com>
2021-04-16 07:30:28 -0700

    fixed the or operator to not be the logical or

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI | "mongodb://localhost/budget";
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 console.log("mongoUri:", mongoUri);
 
